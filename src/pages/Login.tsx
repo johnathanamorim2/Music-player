63d7b3a4c3701241626ecfb37feacd0e0b799b3d
[dyad] <johnathan.amorim2@gmail.com>
2025-10-18 17:22:06 -0300

    [dyad] Removed social login options - wrote 1 file(s)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,7 +25,7 @@ const Login = () => {
         <Auth
           supabaseClient={supabase}
           appearance={{ theme: ThemeSupa }}
-          providers={['google', 'github']}
+          providers={[]}
           theme="dark"
         />
       </div>
